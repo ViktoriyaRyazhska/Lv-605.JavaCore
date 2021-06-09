0383bed2e6f7be15b3a87feac3d45d3d54bf3e58
Geneue <34031302+Geneue@users.noreply.github.com>
2021-06-09 18:02:57 +0300

    Geneue folder added

---
+++ Geneue/Lesson01/lesson01_homework01.java
@@ -0,0 +1,16 @@
+
+public class lesson01_homework01 {
+
+	public static void main(String[] args) {
+		
+		double radius = 1.6;
+		
+		double area = 3.1416 * radius * radius;
+		
+		double circumference = 2 * 3.1416 * radius;
+		
+		System.out.println("If the radius of a circle is " + radius + " then it's area = " + area + " and circumference = " +
+		circumference + '.');
+
+	}
+}
---
+++ Geneue/Lesson01/lesson01_homework02.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class lesson01_homework02 {
+	public static void main (String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Sup, friend. What's your name?");
+		String name = scan.nextLine();
+		System.out.println("Nice to meet you ;) And where do you live, " + name + '?');
+		String adress = scan.nextLine();
+		System.out.println("So you say your name is " + name + " and you live at " + adress + '?');
+		System.out.println("Awesome!");
+
+	}
+}
