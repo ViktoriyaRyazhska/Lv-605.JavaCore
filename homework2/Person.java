6d439a47434f33b5e989e5042b1a8559bee78de0
Pavlo Skolozdra <pavlo.skolozdra@gmail.com>
2021-06-14 15:27:25 +0300

    input from keyboard added

--- homework2/Person.java
+++ homework2/Person.java
@@ -1,5 +1,6 @@
 package homework2;
 
+import java.io.*;
 import java.util.Calendar;
 
 public class Person {
@@ -42,57 +43,64 @@ public class Person {
 		System.out.println(this.firstName + " age is: " + (Calendar.getInstance().get(Calendar.YEAR) - this.birthYear));
 	}
 
-	public void input(String fn) {
-		this.firstName = fn;
+	public void input() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter persons first name: ");
+		try {
+			this.firstName = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Please enter persons last name: ");
+		try {
+			this.lastName = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Please enter persons year of birth: ");
+		try {
+			this.birthYear = Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void input(String fn, String ln) {
-		this.firstName = fn;
-		this.lastName = ln;
-	}
-
-	public void input(String fn, String ln, int by) {
-		this.firstName = fn;
-		this.lastName = ln;
-		this.birthYear = by;
-	}
-	
 	public void output() {
 		System.out.println("Person name is: " + this.firstName + " " + this.lastName + ".");
 		System.out.println("Year of birth: " + this.birthYear);
 		this.getAge();
 	}
-	
+
 	public void changeName(String fn) {
 		this.firstName = fn;
 	}
-	
+
 	public void changeName(String fn, String ln) {
 		this.firstName = fn;
 		this.lastName = ln;
 	}
-
-	public static void main(String[] args) {
-		Person per = new Person("Pavlo", "Skolozdra");
-		per.setBirthYear(1991);
-		per.getAge();
-		per.output();
-		per.input("Petro",  "Teteruk", 1994);
-		per.output();
-		per.changeName("Igor", "Smelyanets");
-		per.output();
-		Person per2 = new Person("Olena", "Petrenko");
-		per2.setBirthYear(1980);
-		Person per3 = new Person("Iryna", "Kosyk");
-		per3.setBirthYear(1995);
-		Person per4 = new Person("Marta", "Karpinska");
-		per4.setBirthYear(1958);
-		Person per5 = new Person("Oleg", "Terletskiy");
-		per5.setBirthYear(1966);
-		per2.output();
-		per3.output();
-		per4.output();
-		per5.output();
+	
+	public static Person getPerson() {
+		return new Person();
 	}
 
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		Person person2 = new Person();
+		Person person3 = new Person();
+		Person person4 = new Person();
+		Person person5 = new Person();
+		
+		person1.input();
+		person2.input();
+		person3.input();
+		person4.input();
+		person5.input();
+		
+		person1.output();
+		person2.output();
+		person3.output();
+		person4.output();
+		person5.output();
+	}
 }
