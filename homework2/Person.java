22f1d7d651cab176fe96bdd9401fc7d1888cd2ec
AlexDanihevych <danifromalex@gmail.com>
2021-06-15 20:10:50 +0300

    corrected homewrok2

--- homework2/Person.java
+++ homework2/Person.java
@@ -3,6 +3,7 @@ package homework2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 
 public class Person {
 
@@ -46,11 +47,10 @@ public class Person {
 	// Methods
 	
 	public int getAge() {
-		return 2021 - birthYear;
+		return LocalDate.now().getYear() - birthYear;
 	}
 	
 	public void changeName (String fn, String ln) {
-		
 		this.firstName = fn;
 		this.lastName = ln;
 	}
