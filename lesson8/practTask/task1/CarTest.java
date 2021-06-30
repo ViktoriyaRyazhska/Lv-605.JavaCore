e30d33f08670b05c56688aad5c0d8a891db0ea52
Roman Klymus <klimusr@gmail.com>
2021-07-01 01:47:19 +0300

    PractTask 1 completed

--- lesson8/practTask/task1/CarTest.java
+++ lesson8/practTask/task1/CarTest.java
@@ -1,10 +1,13 @@
 package lesson8.practTask.task1;
 
+import java.time.LocalDate;
+
 public class CarTest {
 
 	public static void main(String[] args) {
-		Car myCar = new Car.CarBuilder().addColor("Black").addDateOfProduction(null).addEngineCapacity(4000)
-				.addModel("BMW").addPassengerCapacity(4).setIsAirConditioning(true).build();
+		Car myCar = new Car.CarBuilder().addColor("Black").addDateOfProduction(LocalDate.of(1990, 10, 12))
+				.addEngineCapacity(4000).addModel("BMW").addPassengerCapacity(4).setIsAirConditioning(true).build();
+
 		System.out.println(myCar);
 	}
 
