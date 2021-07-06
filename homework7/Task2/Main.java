770519a75eee00117dfe10d3f15af856daa15fc0
Pavlo Skolozdra <pavlo.skolozdra@gmail.com>
2021-07-07 01:20:38 +0300

    Task2

---
+++ homework7/Task2/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Wrapper<Integer> intWrapper = new Wrapper<Integer>();
+		intWrapper.setValue(10);
+		Wrapper<String> stringWrapper = new Wrapper<String>();
+		stringWrapper.setValue("Some text");
+		Wrapper<Boolean> boolWrapper = new Wrapper<Boolean>();
+		boolWrapper.setValue(true);
+		System.out.println(intWrapper.getValue());
+		System.out.println(stringWrapper.getValue());
+		System.out.println(boolWrapper.getValue());
+	}
+
+}
