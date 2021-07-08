5885d9534651091e7c469eccc87558c45cf2be45
Pavlo Skolozdra <pavlo.skolozdra@gmail.com>
2021-07-08 18:49:35 +0300

    person removal to HW2 added

--- homework8/Homework2.java
+++ homework8/Homework2.java
@@ -1,7 +1,6 @@
 package homework8;
 
 import java.util.*;
-import java.util.Map.Entry;
 
 public class Homework2 {
 	public static void main(String[] args) {
@@ -20,22 +19,16 @@ public class Homework2 {
 		myMap.put("Manson", "Marlyn");
 		myMap.put("Kidman", "Nicole");
 
-		// Testing different ways of output
-		System.out.println(myMap);
-		System.out.println(myMap.entrySet());
-		Iterator<Entry<String, String>> it = myMap.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry entry = (Map.Entry) it.next();
-			System.out.println(entry.getKey() + " - " + entry.getValue());
-		}
-		System.out.println("=========================================");
 		for (Map.Entry entry : myMap.entrySet()) {
 			System.out.println(entry.getKey() + " - " + entry.getValue());
 		}
 
 		// There are at least two persons with the same firstName among these 10 people?
 		System.out.println(checkDuplicateValues(myMap));
-
+		System.out.println("======================================");
+		//Remove Orest ))) <^_^>
+		removePersonWithTheFirstName(myMap, "Iggie");
+		System.out.println(myMap);
 	}
 
 	public static boolean checkDuplicateValues(Map<String, String> map) {
@@ -45,5 +38,16 @@ public class Homework2 {
 			return true;
 		}
 		return false;
+	}
+	
+	public static Map<String, String> removePersonWithTheFirstName(Map<String, String> map, String name) {
+		Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
+			if(entry.getValue().equals(name)) {
+				it.remove();
+			}
+		}
+		return map;
 	}
 }
