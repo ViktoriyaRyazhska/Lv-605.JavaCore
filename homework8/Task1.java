29ba1d7ac2b2162c8ae7fd2c636ec9387b50c147
Pavlo Skolozdra <pavlo.skolozdra@gmail.com>
2021-07-08 11:35:03 +0300

    homework8 Task1

---
+++ homework8/Task1.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+
+public class Task1 {
+	public static void main(String[] args) {
+		List<Integer> myCollection = new ArrayList<>();
+		Random rnd = new Random();
+		for (int i = 0; i < 10; i++) {
+			myCollection.add(rnd.nextInt(40));
+		}
+		System.out.println(myCollection);
+		
+		List<Integer> newCollection = new ArrayList<>();
+		ListIterator<Integer> it = myCollection.listIterator();
+		int currentPosition = 0;
+		while (it.hasNext()) {
+			if (it.next() > 5) {
+				newCollection.add(currentPosition);
+			}
+			currentPosition++;
+		}
+		System.out.println(newCollection);
+		
+		it = myCollection.listIterator(); // iterator reset
+		while (it.hasNext()) {
+			if (it.next() > 20) {
+				it.remove();
+			}
+		}
+		System.out.println(myCollection);
+		
+		//insertion of specific elements
+		Task1.addIfPossible(myCollection, 2, 1);
+		Task1.addIfPossible(myCollection, 8, -3);
+		Task1.addIfPossible(myCollection, 5, -4);
+		System.out.println(myCollection);
+		
+		//Sorting and print
+		myCollection.sort(Comparator.naturalOrder());
+		//Collections.sort(myCollection);
+		System.out.println(myCollection);
+	}
+	
+	public static List<Integer> addIfPossible(List<Integer> list, int index, int value) {
+		if(index > list.size() || index < 0) {
+			System.err.println("Index is out of range!");
+			throw new IndexOutOfBoundsException();   //???
+		}
+		else {
+			list.add(index, value);
+		}
+		return list;
+	}
+}
