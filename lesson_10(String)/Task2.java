4c5fa6ba524645f1bc3d1aa445c93656788f7c6d
AlexDanihevych <danifromalex@gmail.com>
2021-07-24 16:46:52 +0300

    added HM10

---
+++ lesson_10(String)/Task2.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Task2 {
+	
+	public static void main(String[] args) {
+		
+		  String monday = "A  ll peo  ple are hating Mo  ndays";		 
+		  System.out.println(monday.replaceAll("[ ]{2,}", ""));
+		 
+	}
+}
+ 
