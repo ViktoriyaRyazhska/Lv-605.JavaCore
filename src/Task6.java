a39afa0f863f36f522d3bd328e2d767055078245
Eugene Voronko <34031302+Geneue@users.noreply.github.com>
2021-07-28 18:58:30 +0300

    added 6, 7, 8 tasks

---
+++ src/Task6.java
@@ -0,0 +1,37 @@
+package softserve.academy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task6 {
+
+	public static void main(String[] args) {
+		System.out.println("Task 6 is starting");
+        System.out.println("Input text and calculate number of words in this text");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str= null;
+		int count = 0;
+		System.out.println("Enter word ");
+		try {
+			str = br.readLine();
+			}catch (IOException a) {
+
+			       System.out.println("Wrong");
+
+			}
+        
+        if(str.length() != 0){
+            count++;
+            
+            for (int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == ' '){
+                    
+                    count++;
+                }
+            }
+        }
+        System.out.println("You enter " + count + "word " );
+        }
+}
+     
---
+++ src/Task7.java
@@ -0,0 +1,40 @@
+package softserve.academy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task7 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("Task 7 is starting");
+		System.out.println("Input number n and calculate n! (e.g. 5! = 1*2*3*4*5 = 120)");
+		System.out.println("======================================================");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = 0;
+		System.out.println("Enter numbers");
+		try {
+			number = Integer.parseInt(br.readLine());
+			}catch (IOException a) {
+
+			       System.out.println("Wrong");
+
+			}
+		System.out.println(number);
+		int product = 1;
+		int y = 1;
+		if (number != 0) {
+		 for (int i = 0; i < number; i++) {
+			
+			product = product * y;
+			y++;
+		}
+		}
+		 System.out.println("answer is " + product);
+		 
+		
+		
+		
+	}
+	
+
+}
