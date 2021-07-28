0dc95cef3fb48fec596f5cf66e4fa30c10a165e5
AlexDanihevych <danifromalex@gmail.com>
2021-07-28 17:49:27 +0300

    changed TS1

--- challange/Task1.java
+++ challange/Task1.java
@@ -1,24 +1,35 @@
-package challange;
+package challenge;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Task1 {
-
-	// 4. Input string and calculate number of loud letters (e.g. word: “My test
-	// text”, result: 3)
-	public static void main(String[] args) throws IOException {
-		String S = "aeyuio";
+	
+	
+	static	int n1 = 0;
+	static	int n2 = 1;
+	int sum =0;
+	public static void generFi (int inputNumber ) {
+		
+		for (int i=0; i<inputNumber; i++) {
+			int sum = n1 +n2;
+			System.out.println(sum + " ");
+			n1 = n2;
+			n2 = sum;
+			
+	}		
+}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String text = br.readLine();
-		int counter = 0;
-		for (String b : text.split(""))
-			if (S.contains(b)) {
-				counter++;
-			}
-		System.out.println("number of loud letters " + counter);
-
+		System.out.println("Enter the number for Fibonacci sequence");
+		int inputNumber = Integer.parseInt(br.readLine());
+		generFi(inputNumber);
+		
+		
 	}
-
+	
+	
 }
